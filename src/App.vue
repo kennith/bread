a393c0905df906666e6da37b589be9f48bcd677c
Kennith Leung <kennith.leung@gmail.com>
2026-02-27 18:07:34 -0800

    feat: implement fetching logic for English and Traditional Chinese devotionals

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,8 @@
 import { onMounted, ref } from 'vue';
 import Title from '@/components/TitleComponent.vue';
 import Section from '@/components/SectionComponent.vue';
-
-const odbApiUrl = 'https://api.experience.odb.org/devotionals/v2';
-const englishSiteId = 1;
-const traditioalChineseSiteId = 7;
-
-const todayUnixTimestamp = new Date().setHours(0, 0, 0, 0);
-
-const englishUrl = new URL(odbApiUrl);
-englishUrl.searchParams.append('site_id', englishSiteId.toString());
-englishUrl.searchParams.append('status', 'publish');
-englishUrl.searchParams.append('country', 'US');
-englishUrl.searchParams.append('on', todayUnixTimestamp.toString());
-
-const traditioalChineseUrl = new URL(odbApiUrl);
-traditioalChineseUrl.searchParams.append('site_id', traditioalChineseSiteId.toString());
-traditioalChineseUrl.searchParams.append('status', 'publish');
-traditioalChineseUrl.searchParams.append('country', 'US');
-traditioalChineseUrl.searchParams.append('on', todayUnixTimestamp.toString());
+import { fetchTraditioalChineseDevotional, fetchEnglishDevotional } from '@/composable/useFetchDevotional.vue';
+import { fetchBible } from '@/composable/useBible.vue';
 
 type Devotional = {
   title: string;
@@ -27,81 +11,32 @@ type Devotional = {
   content: string;
   thought: string;
   response: string;
+  passage_reference: string;
+  lang_author_name: string;
+  insights: string;
 }
 
-const traditionalChinese = ref<Devotional>({
+const devotional = {
   title: '',
   verse: '',
   content: '',
   thought: '',
   response: '',
-});
+  passage_reference: '',
+  lang_author_name: '',
+  insights: '',
+}
 
-const english = ref<Devotional>({
-  title: '',
-  verse: '',
-  content: '',
-  thought: '',
-  response: '',
-});
+const traditionalChinese = ref<Devotional>(devotional);
+const english = ref<Devotional>(devotional);
+
 const bible = ref();
 
-
 onMounted(async () => {
-  fetchEnglishDevotional();
-  fetchTraditioalChineseDevotional();
+  [english.value] = await fetchEnglishDevotional();
+  [traditionalChinese.value] = await fetchTraditioalChineseDevotional();
+  bible.value = await fetchBible(english.value.passage_reference);
 })
-
-const fetchTraditioalChineseDevotional = async () => {
-  try {
-    const response = await fetch(traditioalChineseUrl.toString());
-    [traditionalChinese.value] = await response.json();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-const fetchEnglishDevotional = async () => {
-  try {
-    const response = await fetch(englishUrl.toString());
-    [english.value] = await response.json();
-
-    fetchBible(english.value.passage_reference);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-const fetchVerse = async (bibleId: string, passage: string) => {
-  const odbBibleUrl = new URL('https://www.odbm.org/api/bible/search');
-  odbBibleUrl.searchParams.append('bibleId', bibleId);
-  odbBibleUrl.searchParams.append('query', passage);
-
-  const response = await fetch(odbBibleUrl.toString());
-
-  return response;
-
-}
-
-const fetchBible = async (passageReference: string) => {
-  try {
-    const cun = "c44765fbdfdb0ed9-01"; // CUN
-    const niv = '71c6eab17ae5b667-01';
-
-    let response = await fetchVerse(cun, passageReference);
-
-    if (response.status == 500) {
-      response = await fetchVerse(niv, passageReference);
-    }
-
-    const bibleData = await response.json();
-
-
-    bible.value = bibleData.data.passages;
-  } catch (error) {
-    console.log(error);
-  }
-}
 
 </script>
 
---
+++ src/composable/useFetchDevotional.vue
@@ -0,0 +1,43 @@
+<script lang="ts">
+const odbApiUrl = 'https://api.experience.odb.org/devotionals/v2/';
+const todayUnixTimestamp = new Date().setHours(0, 0, 0, 0);
+
+const fetchTraditioalChineseDevotional = async () => {
+  const traditioalChineseSiteId = 7;
+  const traditioalChineseUrl = new URL(odbApiUrl);
+  traditioalChineseUrl.searchParams.append('site_id', traditioalChineseSiteId.toString());
+  traditioalChineseUrl.searchParams.append('status', 'publish');
+  traditioalChineseUrl.searchParams.append('country', 'US');
+  traditioalChineseUrl.searchParams.append('on', todayUnixTimestamp.toString());
+
+  try {
+    const response = await fetch(traditioalChineseUrl.toString());
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const fetchEnglishDevotional = async () => {
+  const englishUrl = new URL(odbApiUrl);
+  const englishSiteId = 1;
+
+  englishUrl.searchParams.append('site_id', englishSiteId.toString());
+  englishUrl.searchParams.append('status', 'publish');
+  englishUrl.searchParams.append('country', 'US');
+  englishUrl.searchParams.append('on', todayUnixTimestamp.toString());
+
+  try {
+    const response = await fetch(englishUrl.toString());
+    return await response.json();
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+export { fetchTraditioalChineseDevotional, fetchEnglishDevotional };
+</script>
